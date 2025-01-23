c3169305c7256b9330fd0c86ee325fbf7cf6c6a3
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2025-01-23 10:51:59 +0000

    Deploying to gh-pages from @ TetyanaDubchak/Domaine-de-Tavernel@f11baca3d080616568e95500d458b335b75cc0ee 🚀

--- static/js/175.336c1027.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[175],{6175:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("main",{children:(0,n.jsx)("h2",{children:"About"})})}}}]);
-//# sourceMappingURL=175.336c1027.chunk.js.map
